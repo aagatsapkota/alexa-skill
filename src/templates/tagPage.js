9f5d062d9880c2bb096c7b5f54671537fa7d1fee
Aagat Sapkota <ars9@williams.edu>
2020-07-16 10:21:58 -0400

    testing with just one markdown query, it works

--- src/templates/tagPage.js
+++ src/templates/tagPage.js
@@ -1,7 +1,25 @@
 import React from 'react'
+import { graphql } from 'gatsby'
 
-export default function tagPage() {
+export const mdQuery = graphql`
+{
+  markdownRemark {
+    frontmatter {
+      tags
+    }
+  }
+}
+`
+export default function tagPage({
+  data, // this prop will be injected by the GraphQL above
+}) {
+  const { markdownRemark } = data // data.markdownRemark holds your post data
+  const { frontmatter } = markdownRemark
   return (
-    <div>Template for Tag Pages</div>
+    <div>
+      <h1>{JSON.stringify(data.markdownRemark)}</h1>
+      <br />
+      {frontmatter.tags}
+    </div>
   )
 }
